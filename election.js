b5a53baa653e79fe40c909659cdd6b5aa47310a4
emanuelsamel <mannys94@hotmail.com>
2017-07-12 16:31:55 -0400

    bikini part 1

--- election.js
+++ election.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  $.ajax({
+    url:"https://bb-election-api.herokuapp.com/",
+    method:"GET",
+    dataType:"JSON"
+  })
+  .done(function(data){
 
+    for(var i = 0; i < data.candidates.length; i++){
+      var candidates = document.createTextNode(data.candidates[i].name + "  :  "  + data.candidates[i].votes);
+      var li = document.createElement("li");
+      li.append(candidates);
+      var election = document.querySelector("#election");
+      election.append(li);
+    }
+  })
 });
